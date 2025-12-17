672264c59c93ee378284407f0527c1fab537825c
TrotmanCH <charlytr207@gmail.com>
2025-12-16 21:31:09 -0500

    Subiendo migraciones

---
+++ database/migrations/2025_12_17_021233_create_comments_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->string('user_comment');
+            $table->text('comment');
+            $table->foreignId('id_post')
+                  ->constrained('posts')
+                  ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comments');
+    }
+};
